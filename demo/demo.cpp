a1ac4f590bef9f5e6cb6786b43b4aa28cb90794c
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-02-12 16:43:30 +0700

    commit funny

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
 using namespace std;
 
+struct node
+{
+  int data;
+  node *next;
+  node *pre;
+};
+
+struct douList
+{
+  node *head;
+  node *tail;
+};
+
+douList *initList(int x)
+{
+  douList *l = new douList;
+  l->head = new node;
+  l->head->data = x;
+  l->head->pre = NULL;
+  l->head->next = NULL;
+  l->tail = l->head;
+  return l;
+}
+
+douList *addHead(douList *l, int x)
+{
+  node *temp = new node;
+  temp->data = x;
+  temp->pre = NULL;
+  temp->next = l->head;
+  l->head->pre = temp;
+  l->head = temp;
+  return l;
+}
+
+douList *addTail(douList *l, int x)
+{
+  node *temp = new node;
+  temp->data = x;
+  temp->next = NULL;
+  temp->pre = l->tail;
+  l->tail->next = temp;
+  l->tail = temp;
+  return l;
+}
+
+void printList(douList *l)
+{
+  for (node *i = l->head; i != NULL; i = i->next)
+  {
+    cout << i->data << " ";
+  }
+}
+
 int main()
 {
-  cout << 1.0 * 2 / 5;
+  int n;
+  cin >> n;
+  douList *l = initList(1);
+  for (int i = 2; i <= n; i++)
+  {
+    l = addHead(l, i);
+    l = addTail(l, i);
+  }
+  printList(l);
   return 0;
 }
