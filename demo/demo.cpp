39d684d5619217173d0accaf349bdf6337fc45df
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-03-01 18:10:02 +0700

    update save code

---
+++ demo/demo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i += n / 3)
+  {
+    for (int j = n; j < n; j++)
+    {
+      //
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 1; j < n; j *= 2)
+    {
+      //
+    }
+  }
+  return 0;
+}
