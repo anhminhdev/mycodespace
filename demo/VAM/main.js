1dc4fbade3fe6e6a4ae17faf5711b6b83b70c52f
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-03-06 21:51:41 +0700

    update

--- demo/VAM/main.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')()
-
-var firstName = prompt('Enter your first name: ');
-var lastName = prompt('Enter your last name: ')
-
-console.log('Your full name is:', firstName, lastName) 
---
+++ demo/js/app.js
@@ -0,0 +1,4 @@
+let headingNode = document.querySelector('.box .heading-2:nth-child(1)')
+console.log(headingNode)
+
+console.log(document.forms())
